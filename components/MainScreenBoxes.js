e6d532752ad7f011c82f45e37096e16717c754d9
Callum Powley <callum.powley@gmail.com>
2021-05-13 17:43:37 +0100

    Added boxes

--- components/MainScreenBoxes.js
+++ components/MainScreenBoxes.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import styles from './flex.module.css'
 
-function BoxComponent({ children, name }) {
+function BoxComponent({ children, name, image }) {
   return <div className={styles.flexItem}>{children}</div>
 }
 
 const MainScreenBoxes = () => {
   return (
     <div className={styles.flexContainer}>
-      <BoxComponent name="lord queef">MEMES</BoxComponent>
+      <BoxComponent name="lord queef">TEST</BoxComponent>
+      <BoxComponent name="lord queef">TEST</BoxComponent>
+      <BoxComponent name="lord queef">TEST</BoxComponent>
+      <BoxComponent name="lord queef">TEST</BoxComponent>
     </div>
   )
 }
